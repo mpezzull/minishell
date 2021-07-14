fb5ab93fdf408505251f0f02136939e6fb742405
mde-rosa <mario.de.rosa@hotmail.it>
2021-07-14 05:11:57 +0200

    ft_echo da testare bene

--- bin/ft_echo.c
+++ bin/ft_echo.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   our_echo.c                                         :+:      :+:    :+:   */
+/*   ft_echo.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mde-rosa <mde-rosa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/28 11:54:05 by assokenay         #+#    #+#             */
-/*   Updated: 2021/07/13 17:45:41 by mde-rosa         ###   ########.fr       */
+/*   Updated: 2021/07/14 02:58:19 by mde-rosa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,66 @@
 #include <unistd.h>
 
 
-//echo -naaa ciao a tutti
+//ritorna il numero di caratteri letti, (-1 se trova solo il '-'), 0 se la stringa non contiene opzioni!
+int	check_options(char *str, char option)
+{
+	int index;
 
+	index = 0;
+	if (!str)
+		return (0);
+	if (str[index] == '-')
+		index++;
+	else
+		return (0);
+	while (str[index] == option)
+		index++;
+	if (!str[index] == '\0')
+		return (0);
+	if (index == 1)
+		return (-1);
+	return(index);
+}
+/*
+./ft_echo -n -n -n -n ciao | cat -e
+argc = 6;
+
+*/
 int	main(int argc, char **argv)
 {
-	int	option;
 	int	i;
+	int	option;
 
 	i = 1;
-	if (argc == 1)
+	option = 0;
+	if (argc > 1)
 	{
-		printf("\n");
-		return (1);
+		if (argv[1][0] == '-' && argv[1][1] == '\0')
+		{
+			i++;
+			option = -1;
+		}
+		if (option == 0)
+		{	
+			while (check_options(argv[i], 'n') > 1)
+				i++;
+		}
+		if (i > 1 && option == 0)
+			option = 1;
+		while (i < argc)
+		{
+			printf("%s", argv[i]);
+			if (i < (argc - 1))
+				printf(" ");
+			i++;
+		}
 	}
+	if (option <= 0)
+		printf("\n");
+	return (0);
+}
+		
+/*
 	if (argv[1][0] == '-' && argv[1][1] == 'n' && argv[1][2] != ' ')
 	{
 		option = 1;
@@ -42,4 +89,5 @@ int	main(int argc, char **argv)
 		i++;
 	}
 	return (0);
-}
+	*/
+
